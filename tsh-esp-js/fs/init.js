ddd2d0013fb0008ab3f173243c410cb438e6d34c
Anton <tonchik@tonchik.ru>
2020-01-18 13:41:36 +0300

    updated version of init with some bugfixes

--- tsh-esp-js/fs/init.js
+++ tsh-esp-js/fs/init.js
@@ -41,15 +41,21 @@ let MQTT_dev = '/devices/' + Cfg.get('device.id');
 let lastConfig = "never";
 let lastWiFi_GOT_IP = -1;
 
+let tshDebug = 0;
+    load('api_esp32.js');
+    load('api_esp32_touchpad.js');
+ 
+//    load('api_esp8266.js');
+
 // FYI
 let device = "unknown";
 RPC.call(RPC.LOCAL, 'Sys.GetInfo', null, function(resp, ud) {
   device = resp.arch;
-  if ( device === "esp32" )
-	load('api_esp32.js');
+ /* if ( device === "esp32" )
+  {
+ }
   if ( device === "esp8266" ) 
-	load('api_esp8266.js');
-
+*/
 },null);
 
 // Common
@@ -57,7 +63,8 @@ let MQTT_publish = function (component, data) {
     let msg = JSON.stringify (data);
     let topic = MQTT_dev + component;
     let ok = MQTT.pub(topic, msg, 1);
-    print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', msg); 
+    if (tshDebug !== 0 || ok === 0) 
+        print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', msg); 
 };
 
 let GetStats = function () {
@@ -91,39 +98,34 @@ runStatusReporter(1000);
 MQTT.sub(MQTT_dev + '/status/config', function(conn, topic, msg) {
   print('Topic:', topic, 'message:', msg);
   let o = JSON.parse(msg);
-  if (lastConfig === "never") {
-    runStatusReporter(
-	(o.interval !== null) ? o.interval*1000 : 5000
-    );
-  }
+  if (o.interval === undefined)
+    o.interval = 5;
+  runStatusReporter(o.interval * 1000);
   lastConfig = o.date;
 }, null);
 
 
 // DHT global
 
-let DHTt = -128;
-let DHTh = -128;
-let DHTpin = -1;
-let dht = null;
-
+let dhts = {} ;
 
 let DHT_init = function(pin, interval) {
   print("Initializing DHT2302 @ ", pin);
   
-  if (dht !== null && DHTpin !== -1) {
-      print("already done @",DHTpin,"reboot to set new one");
+  if (dhts[pin] !== undefined) {
+      print("already done @",pin,"reboot to reset");
       return;
   }
   
-  DHTpin = pin;
-  dht = DHT.create(DHTpin, DHT.AM2302);
-  //dht.begin();
+  dhts[pin] = DHT.create(pin, DHT.AM2302);
+  dhts[pin].begin();
 
-  Timer.set(interval, true , function() {
+  Timer.set(interval, true , function(p) {
 
-    let h = dht.getHumidity();
-    let t = dht.getTemp();
+    let h = dhts[p.DHTpin].getHumidity();
+    let t = dhts[p.DHTpin].getTemp();
+    let DHTt = -128;
+    let DHTh = -128;
 
     if (!isNaN(t) )
       DHTt = t;
@@ -135,36 +137,34 @@ let DHT_init = function(pin, interval) {
       {
         "temp": DHTt,
         "hum": DHTh,
-        "pin": DHTpin
+        "pin": p.DHTpin
       }
     );
-  }, null);
-  print("Initializing DHT2302 @ ", DHTpin, " - done");
+  }, {DHTpin: pin});
+
+  print("Initializing DHT2302 @ ", pin, " - done");
 };
 
 
 // ADC Part
 
-let ADCpin = -1;
-
 let ADC_init = function(pin, interval) {
   print("Initializing ADC @ ", pin);
 
-  ADCpin = pin;
-  
-  ADC.enable(ADCpin);
+  if ( ADC.enable(pin) === 0)
+	print("Enabling ADC @ ", pin, " failed");
 
-  Timer.set(interval, true , function() {
+  Timer.set(interval, true , function(p) {
     MQTT_publish(
       "/ADC",
       {
-        "RAWvalue":  ADC.read(ADCpin),
-        "pin": ADCpin
+        "RAWvalue":  ADC.read(p.ADCpin),
+        "pin": p.ADCpin
       }
     );
-  }, null);
+  }, {ADCpin: pin});
   
-  print("Initializing ADC @ ", ADCpin, " - done");
+  print("Initializing ADC @ ", pin, " - done");
 };
 
 // GPIO Related
@@ -328,7 +328,78 @@ MQTT.sub(MQTT_dev + '/MHZ19/init', function(conn, topic, msg) {
   let o = JSON.parse(msg);
   MHZ19_init(o.uartNo,o.rxto,o.txto,o.interval);
 }, null);
+/*
+let touchInited = false;
+let TPads = {};
 
+let touch_clearReleaseTimer = function(ts){
+  if (TPads[ts].release.timer !== null) {
+    Timer.del(TPads[ts].release.timer);
+    TPads[ts].release.timer = null;
+  }
+};
+
+let touch_release = function(ts) {
+  print('Touchpad',ts,'released');
+  TPads[ts].state = false;
+  touch_clearReleaseTimer(ts);
+};
+
+let touch_press = function(ts) {
+  print('Touchpad',ts,'pressed');
+  TPads[ts].state = true;
+  TPads[ts].release.timer = Timer.set(TPads[ts].release.timeout, 0, touch_release, ts);
+};
+
+let touch_handler = function(st, cfg) {
+  for (let ts = 0; ts <= 9; ts++) {
+    let f = (1 << ts) & st;
+    if ( (f > 0) && TPads[ts].inited) {
+      let val = TouchPad.readFiltered(ts);
+      print('Touch #', ts, 'Value:', val);
+      if (TPads[ts].state) 
+         touch_clearReleaseTimer(ts);
+      touch_press(ts);      
+    }
+  }
+  TouchPad.clearStatus();
+};
+
+let touch_gInit = function() {
+  if (touchInited) return false;
+  print ('Touchpad global init');
+  TouchPad.init();
+  TouchPad.filterStart(10);
+  TouchPad.setMeasTime(0x1000, 0xffff);
+  TouchPad.setVoltage(TouchPad.HVOLT_2V4, TouchPad.LVOLT_0V8, TouchPad.HVOLT_ATTEN_1V5);
+  TouchPad.isrRegister(touch_handler,null);
+  TouchPad.intrEnable();
+  return (touchInited = true);
+};
+
+let touch_configPin = function (ts) {
+  TPads[ts].noTouchVal = TouchPad.readFiltered(ts);
+  TPads[ts].touchThresh = TPads[ts].noTouchVal *  TPads[ts].sens;
+  TouchPad.setThresh(ts, TPads[ts].touchThresh);
+  print('Touch Sensor', ts, 'noTouchVal', TPads[ts].noTouchVal, 'touchThresh', TPads[ts].touchThresh);
+  TPads[ts].inited = true;      
+};
+
+let touch_startPin = function(ts) {
+  touch_gInit();
+  print('Touch Sensor', ts,'init');
+  TouchPad.config(ts, 0);
+  TPads[ts] = {
+    'sens': 0.8,
+    'release' : { 'timeout': 200, "t": null},
+    'state': false,
+    'inited': false,
+  };
+  Timer.set(1000, 0, touch_configPin, ts); 
+};
+
+//touch_startPin(0);
+*/
 
 
 
