d8dbbd9aa535476a23b607d86f555c29f1731b16
Anton Kaukin <tonchik@tonchik.ru>
2023-02-20 20:31:47 +0300

    Skeleton for ESP32 PCNT

---
+++ tsh-esp-js/esp32/src/esp32hwpcnt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "mgos.h"
+#include "esp32hwpcnt.h"
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/portmacro.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "driver/periph_ctrl.h"
+#include "driver/gpio.h"
+#include "driver/pcnt.h"
+#include "esp_attr.h"
+#include "esp_log.h"
+#include "soc/gpio_sig_map.h"
+
+bool mgos_mongoose_os_esp32hwpcnt_init(void) {
+    return true;
+}
